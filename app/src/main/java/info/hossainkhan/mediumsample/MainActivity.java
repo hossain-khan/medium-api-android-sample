9fe8ae7fe453be70d0537bb24071fffecacf0766
Hossain Khan <amardeshbd@gmail.com>
2016-06-26 20:44:44 -0400

    [ADDED] Module for saving generated android client.

--- app/src/main/java/info/hossainkhan/mediumsample/MainActivity.java
+++ app/src/main/java/info/hossainkhan/mediumsample/MainActivity.java
@@ -3,6 +3,7 @@ package info.hossainkhan.mediumsample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
